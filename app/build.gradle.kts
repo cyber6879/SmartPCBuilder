8c65abeeaa2c28b04b5304da034a4a16c313491b
cyber6879 <boyuanyao2022@gmail.com>
2026-09-19 08:23:41 +0800

    Bump SmartPCBuilder to v0.6.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.ikun.smartpc"
         minSdk = 24
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.6.2"
+        versionCode = 9
+        versionName = "0.6.3"
     }
 
     buildTypes {
